5b922b96e435659373c2314dc152f8a46d387350
mamekelle <mamekelle@gmail.com>
2020-10-24 21:53:41 -0500

    :construction: Modification to add validator for ZipcodePair and Main class for accepting user input from environment variables.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,17 @@
-import javax.validation.constraints.NotNull;
+import com.ws.zipcode.service.ParseValidateArg;
+import com.ws.zipcode.service.ZipCodeService;
+import com.ws.zipcode.serviceImp.ParseValidateArgImpl;
+import com.ws.zipcode.serviceImp.ZipCodeServiceImpl;
+
 
 public class Main {
-    @NotNull(message = "{null.message}")
-    static String str;
-    public static void main(String[] args) {
 
-        System.out.println(str);
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        if (args == null || args.length == 0) {
+            throw new IllegalArgumentException("Given Zipcode range is empty!");
+        }
+        ParseValidateArg parseValidateArg = new ParseValidateArgImpl();
+        ZipCodeService zipCodeService = new ZipCodeServiceImpl();
+        zipCodeService.restrictedZipRange(parseValidateArg.parseAndValidate(args)).forEach(System.out::print);
     }
-
 }
